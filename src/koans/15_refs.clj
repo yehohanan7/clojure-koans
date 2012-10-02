64c6fae8a749be0509570b1150471621af808000
John <yehohanan7@gmail.com>
2012-10-02 16:14:42 +0100

    solutions

--- src/koans/15_refs.clj
+++ src/koans/15_refs.clj
@@ -3,37 +3,38 @@
 
 (meditations
   "In the beginning, there was a word"
-  (= __ (deref the-world))
+  (= "hello" (deref the-world))
 
   "You can get the word more succinctly, but it's the same"
-  (= __ @the-world)
+  (= "hello" @the-world)
 
   "You can be the change you wish to see in the world."
-  (= __ (do
+  (= "better" (do
           (dosync (ref-set the-world "better"))
           @the-world))
 
   "Alter where you need not replace"
-  (= __ (let [exclamator (fn [x] (str x "!"))]
+  (= "better!!!" (let [exclamator (fn [x] (str x "!"))]
           (dosync
-           (alter the-world exclamator)
-           (alter the-world exclamator)
-           (alter the-world exclamator))
+            (alter the-world exclamator)
+            (alter the-world exclamator)
+            (alter the-world exclamator))
           @the-world))
 
   "Don't forget to do your work in a transaction!"
-  (= 0 (do __
-           @the-world))
+  (= 0 (dosync
+         (ref-set the-world 0) @the-world))
 
   "Functions passed to alter may depend on the data in the ref"
   (= 20 (do
-          (dosync (alter the-world ___))))
+          (dosync (alter the-world #((apply comp (take 20 (repeat inc))) %)))))
 
   "Two worlds are better than one"
   (= ["Real Jerry" "Bizarro Jerry"]
-       (do
-         (dosync
-          (ref-set the-world {})
-          (alter the-world assoc :jerry "Real Jerry")
-          (alter bizarro-world assoc :jerry "Bizarro Jerry")
-          __))))
+     (do
+       (dosync
+         (ref-set the-world {})
+         (alter the-world assoc :jerry "Real Jerry")
+         (alter bizarro-world assoc :jerry "Bizarro Jerry"))
+         (concat (vals @the-world)  (vals @bizarro-world)))))
+
